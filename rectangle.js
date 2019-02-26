40aebcefea3e075ad7b8909332c10c834d5a7f97
wangding <408542507@qq.com>
2019-02-26 17:01:40 +0800

    矩形计算器

---
+++ rectangle.js
@@ -0,0 +1,11 @@
+console.log('hello')
+$(function(){
+  var $width = $('#width');
+  var $height = $('#height');
+  var $btn = $('btn');
+ var $perimeter = $('perimeter');
+   var p = 2*$width*$height;
+   $perimeter.val(p)
+
+})
+
